89715fc4a2a9a6266a32ed74009e6dae91ab96bc
Dyr3618 <120258634+Dyr3618@users.noreply.github.com>
2023-08-17 11:52:44 +0530

    Update FbApplication.java

--- src/main/java/in/javahomecloud/fb/FbApplication.java
+++ src/main/java/in/javahomecloud/fb/FbApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+public class HelloApplication {
 public class FbApplication {
-
+	@GetMapping("/getmsg")
+	public String getMsg()
+	{
+		return "Hello Aws !!!!!  this is for testing";
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FbApplication.class, args);
 	}
